a5e279cc77cf4b1c0bf85db630ff3ca80a597003
PinoyBloon <ikhlazoelzephyr@gmail.com>
2023-01-21 16:34:35 +0700

    Main Layout for Settings and Profile

--- app/src/main/java/com/example/rangkul/ui/profile/ProfileFragment.kt
+++ app/src/main/java/com/example/rangkul/ui/profile/ProfileFragment.kt
@@ -2,11 +2,11 @@ package com.example.rangkul.ui.profile
 
 import android.content.Intent
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.appcompat.widget.AppCompatButton
+import android.widget.ImageView
+import androidx.fragment.app.Fragment
 import com.example.rangkul.R
 
 class ProfileFragment : Fragment() {
@@ -22,7 +22,7 @@ class ProfileFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        view.findViewById<AppCompatButton>(R.id.btSettings).setOnClickListener {
+        view.findViewById<ImageView>(R.id.btnSetting).setOnClickListener {
             val intent = Intent(context, SettingsActivity::class.java)
             startActivity(intent)
         }
